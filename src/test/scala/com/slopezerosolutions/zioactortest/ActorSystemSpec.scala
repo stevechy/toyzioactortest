d5f73867c04661cd83f7ca23bbb75d0a7e7aca02
stevechy <stevechy@gmail.com>
2023-03-14 15:29:53 -0600

    Send message to promise

--- src/test/scala/com/slopezerosolutions/zioactortest/ActorSystemSpec.scala
+++ src/test/scala/com/slopezerosolutions/zioactortest/ActorSystemSpec.scala
@@ -1,12 +1,21 @@
 package com.slopezerosolutions.zioactortest
 
+import zio._
 import zio.test._
 
 class ActorSystemSpec extends zio.test.junit.JUnitRunnableSpec {
 
-  def spec = suite("ActorSystem tests")(
+  def spec: Spec[Any, Throwable] = suite("ActorSystem tests")(
     test("can send a message") {
-      assert(true)(Assertion.equalTo(true))
+      val actorSystem = new ActorSystem
+
+      val sendMessageZIO = for{
+        resultPromise <- Promise.make[Throwable, String]
+        destination <- ZIO.succeed(actorSystem.promiseMessageDestination(resultPromise))
+        _ <- actorSystem.send("Hello world", destination)
+        result <- resultPromise.await
+      } yield result
+      assertZIO(sendMessageZIO)(Assertion.equalTo("Hello world"))
     }
   )
 }
